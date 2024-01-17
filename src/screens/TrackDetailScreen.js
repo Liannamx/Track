a5cb00a7923c5287ab65e208664d31d3737ccf66
Alina <Maksimova>
2024-01-17 12:50:26 +0400

    add screens to the project

---
+++ src/screens/TrackDetailScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const TrackDetailSceren = () => { 
+    return <Text style={{ fontSize: 48 }}>TrackDetail Sceren</Text>
+};
+
+const style = StyleSheet.create({});
+
+export default TrackDetailSceren;
---
+++ src/screens/TrackListScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const TrackListSceren = () => { 
+    return <Text style={{ fontSize: 48 }}>TrackList Sceren</Text>
+};
+
+const style = StyleSheet.create({});
+
+export default TrackListSceren;
