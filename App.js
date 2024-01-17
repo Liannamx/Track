ad6385d6b362b24b10135c124671c508f82ee839
Alina <Maksimova>
2024-01-17 14:21:02 +0400

    updated app navigation

--- App.js
+++ App.js
@@ -1,6 +1,27 @@
+import React from "react";
+import { creaateAppContainer, createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
 import AccountScreen from "./src/screens/AccounScreen";
 import SigninScreen from "./src/screens/SigninScreen";
 import SignupScreen from "./src/screens/SignupScreen";
 import TrackCreateScreen from "./src/screens/TrackCreateScreen";
 import TrackDetailSceren from "./src/screens/TrackDetailScreen";
 import TrackListSceren from "./src/screens/TrackListScreen";
+
+const switchNavigator = createSwitchNavigator({
+  loginFlow: createStackNavigator({
+    Signup: SignupScreen,
+    Signin: SigninScreen,
+  }),
+  mainFlow: createMaterialBottomTabNavigator({
+    trackListFlow: createSwitchNavigator({
+      TrackList: TrackListSceren,
+      TrackDetail: TrackDetailSceren,
+    }),
+    TrackCreate: TrackCreateScreen,
+    Account: AccountScreen,
+  }),
+});
+
+export default createAppContainer(switchNavigator);
